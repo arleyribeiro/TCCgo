8c05ce71dbff663bd16f42957434380cdfbb4b68
daegonny <igorchagasm@hotmail.com>
2017-09-24 17:01:15 -0300

    Implementado cadastro

--- TCCgo/TCCgo/apps/authentication/views.py
+++ TCCgo/TCCgo/apps/authentication/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
-# VIEW DE TESTE
+from .controller import (
+    UserController
+)
+
 def index(request):
     return render(request, 'index.html');
 
 def register(request):
     return render(request, 'register.html');
+
+def create_user(request):
+    user_controller = UserController()
+    user_controller.request_create(request)
+    return render(request, 'index.html');
