91c9b413a9aa5560cf4c935b25693629f23a2441
daegonny <igorchagasm@hotmail.com>
2017-10-02 22:39:37 -0300

    Implementado sistema de atualizacao de usuario

--- TCCgo/TCCgo/apps/authentication/controller.py
+++ TCCgo/TCCgo/apps/authentication/controller.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.forms import model_to_dict
 from pprint import pprint
+import json
 from .models import (
     User
 )
@@ -14,24 +16,26 @@ class UserController(object):
         new_user.save()
         return new_user;
 
-    def update(self, session_user_id, username, email, password, enroll_number):
-        current_user = user.objects.get(pk=session_user_id)
+    def update(self, session_user_id, username, email, enroll_number):
+        current_user = User.objects.get(pk=session_user_id)
         if current_user != None:
             current_user.username = username
             current_user.email = email
-            current_user.password = password
             current_user.enroll_number = enroll_number
             current_user.save()
         return current_user
 
     def delete(self, session_user_id):
-        current_user = user.objects.get(pk=session_user_id)
+        current_user = User.objects.get(pk=session_user_id)
         if current_user != None:
             current_user.remove()
             return True
         else:
             return False
 
+    def get_logged_user(self, session_user_id):
+        current_user = User.objects.get(pk=session_user_id)
+        return current_user
 
     def request_create(self, request):
         username = request.POST['username']
@@ -41,21 +45,38 @@ class UserController(object):
         return self.create(username, email, password, enroll_number)
 
     def request_update(self, request):
-        session_user = request.user
-        if session_user != None:
-            session_user_id = session_user.id
-            username = request.POST['username']
-            email = request.POST['email']
-            password = request.POST['password']
-            enroll_number = request.POST['enroll_number']
-            return self.update(session_user_id, username, email, password, enroll_number)
-        else:
-            return None
+        if request.user.is_authenticated :
+            session_user = request.user
+            if session_user != None:
+                session_user_id = session_user.id
+                user_json = json.loads(request.body.decode('utf-8'))
+                username = user_json['username']
+                email = user_json['email']
+                enroll_number = user_json['enroll_number']
+                updated_user =  self.update(session_user_id, username, email, enroll_number)
+                dict_updated_user = model_to_dict(updated_user, fields = ["username", "email", "enroll_number"])
+                return dict_updated_user
+            else:
+                return False
+        else: return False
 
-    def reqeust_delete(self, request):
+    def request_delete(self, request):
         session_user = request.user
         if session_user != None:
             session_user_id = session_user.id
             return self.delete(session_user_id)
         else:
             return None
+
+    def request_get_logged_user(self, request):
+        if request.user.is_authenticated :
+            session_user = request.user
+            if session_user != None:
+                session_user_id = session_user.id
+                logged_user = self.get_logged_user(session_user_id)
+                dict_logged_user = model_to_dict(logged_user, fields = ["username", "email", "enroll_number"])
+                return dict_logged_user
+            else:
+                return False
+        else:
+            return False
--- TCCgo/TCCgo/apps/authentication/views.py
+++ TCCgo/TCCgo/apps/authentication/views.py
@@ -1,9 +1,13 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+from pprint import pprint
 
 from .controller import (
     UserController
@@ -18,6 +22,11 @@ def index(request):
 def register(request):
     return render(request, 'register.html');
 
+def get_logged_user(request):
+    user_controller = UserController()
+    logged_user = user_controller.request_get_logged_user(request)
+    return JsonResponse(logged_user, safe=False)
+
 def create_user(request):
     user_controller = UserController()
     user_controller.request_create(request)
@@ -30,8 +39,8 @@ def delete_user(request):
 
 def update_user(request):
     user_controller = UserController()
-    user_controller.request_delete(request)
-    return render(request, 'index.html');
+    updated_user = user_controller.request_update(request)
+    return JsonResponse(updated_user, safe=False)
 
 def login(request):
 	if request.method == 'POST':
@@ -49,7 +58,6 @@ def login(request):
 			return HttpResponse('Invalid Login'+' '+email+' '+password +' <p> não funciona para rute, a documentação é inimiga da perfeicao')
 	else:
 		return render(request,'login.html')
-
 
 def logout(request):
 	auth_logout(request)
