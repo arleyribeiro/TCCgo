d9cb57edd1d0614186af5f71ee7070655b3b4754
João <joao.psp@hotmail.com>
2017-11-03 15:37:57 -0200

    Forum Inicializado

--- TCCgo/TCCgo/apps/forum/urls.py
+++ TCCgo/TCCgo/apps/forum/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
+    url(r'^$', login_required(views.forum_page), name='forum'),
 
 ]
--- TCCgo/TCCgo/apps/forum/views.py
+++ TCCgo/TCCgo/apps/forum/views.py
@@ -1,3 +1,9 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
+
+def forum_page(request):
+	return render(request, 'forum.html')
